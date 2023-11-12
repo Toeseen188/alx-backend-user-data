69df9ccc0165c3794950369e8bcc2a2e0ca45691
Hammed Tosin <hammedtosin188@gmail.com>
2023-11-12 23:10:18 +0100

    trim token from authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,20 @@ class BasicAuth(Auth):
     """ This class inherit from class Auth
     """
     pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extract the string after 'Basic' in the authorization header
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        token = authorization_header[len('Basic '):].strip()
+        return token
