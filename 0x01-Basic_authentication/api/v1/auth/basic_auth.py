508e1ad4bdeb2bee36555463965bb39edabbd3cb
Hammed Tosin <hammedtosin188@gmail.com>
2023-11-13 14:43:14 +0100

    decode 64base code to string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -26,3 +27,25 @@ class BasicAuth(Auth):
 
         token = authorization_header[len('Basic '):].strip()
         return token
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        extract and decode token from base64
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # try decoding to base64 byte
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+
+            # convert bytes to string
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except Exception as e:
+            return None
