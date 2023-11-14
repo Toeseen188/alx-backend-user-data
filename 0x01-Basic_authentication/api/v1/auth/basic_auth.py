4ad5e73ec85e89d2a177d429103af3056675653b
Hammed Tosin <hammedtosin188@gmail.com>
2023-11-14 19:44:02 +0100

    testing the mic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic Authentication
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -71,3 +73,30 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(":", 1)
 
         return email, password
+
+    def user_object_from_credentials(self,
+                                     user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        takes user objs from credentials(email, password)
+        Return:
+        - User objects
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        user = User()
+        found_users = user.search(user_email)
+
+        if not found_users:
+            return None
+
+        user_ = found_users[0]
+
+        if not user_.is_valid_password(user_pwd):
+            return None
+
+        return user_
