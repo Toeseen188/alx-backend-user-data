df14c806b7d8ec5b804731e38a24b48025881c64
Hammed Tosin <hammedtosin188@gmail.com>
2023-11-20 17:37:24 +0100

    instance method create session id implemented

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,27 @@
 session Authorization
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ THe session class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        that creates a Session ID for a user_id:
+        Return:
+         - None if user_id is None
+         - None if user_id is not a string
+        Otherwise:
+         - Generate a Session ID using uuid module and uuid4() like id in Base
+        Return:
+         - the Session ID
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
